441309032b1a10a1baf1e61a5220d63881407dd7
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-09 21:02:42 -0800

    Print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the alphabet at reverse
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
