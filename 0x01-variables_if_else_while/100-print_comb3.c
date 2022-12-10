4970539864bef866ca3715a7b9382cd7e715a276
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-09 21:28:21 -0800

    Print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
+  * main - Prints combination of numbers
   *
   * Return: 0
   */
@@ -10,19 +10,20 @@ int main(void)
 	int d;
         int i;
 
-	d = i = '0';
-
 	for (d = '0'; d <= '9'; d++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(d);
-			putchar(i);
-
-			if ((d != '9') || (d == '9' && i != '9'))
+			if (d < i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(d);
+				putchar(i);
+
+				if (d != '8' || (d == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
@@ -31,3 +32,4 @@ int main(void)
 
 	return (0);
 }
+
