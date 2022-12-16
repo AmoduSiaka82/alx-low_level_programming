1316b942dead863183b263d211320ba7ba43e358
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-15 19:17:13 -0800

    squre

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 /**
  * print_square - a function that prints a square, followed by a new line
  * @size: size of both width and length
- * Return: a square made of '@'
+ * Return: a square made of '#'
  */
 void print_square(int size)
 {
@@ -17,10 +17,10 @@ void print_square(int size)
 	{
 		for (c = 1; c <= size; c++)
 		{
-			_putchar('@');
+			_putchar('#');
 			for (r = 2; r <= size; r++)
 			{
-				_putchar('@');
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
