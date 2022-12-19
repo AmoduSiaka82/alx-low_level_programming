932398bc8d3eebcd6e43c8dc036e81f9dde96850
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-19 13:33:20 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * puts2 - function should print only one character out of two
@@ -8,12 +8,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int n = 0;
 
-	for (; str[i] != '\0'; i++)
+	for (; str[n] != '\0'; n++)
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
+		if ((n % 2) == 0)
+			_putchar(str[n]);
 		else
 			continue;
 	}
