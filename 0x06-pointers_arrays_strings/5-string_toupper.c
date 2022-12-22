2aed5a4732ccb0c54411b3de4269c670440f505f
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-22 07:43:37 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,10 @@ char *string_toupper(char *n)
 {
 	int i = 0;
 
-	for (; n[i] != '\0'; )
+	for (; n[i] != '\0'; i++)
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
-		 i++
 	}
 	return (n);
 }
