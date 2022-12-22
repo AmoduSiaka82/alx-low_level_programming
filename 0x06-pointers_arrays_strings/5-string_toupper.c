d5c62aeb1bfe5327c49cfd8818862c517b56146c
AmoduSiaka82 <amodusiakanet@gmail.com>
2022-12-22 07:40:18 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - a function that changes all lower of a string to upper
+ * @n: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	for (; n[i] != '\0';)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		 i++
+	}
+	return (n);
+}
