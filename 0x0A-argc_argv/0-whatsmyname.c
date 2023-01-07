199b7415ef9ee517bdd08eb5d63e03831648246c
AmoduSiaka82 <amodusiakanet@gmail.com>
2023-01-06 16:19:13 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
