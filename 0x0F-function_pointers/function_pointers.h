c08451456a58e885365f9157a0e0da2b89868a0c
AmoduSiaka82 <amodusiakanet@gmail.com>
2023-01-18 17:25:54 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 typedef __SIZE_TYPE__ size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int putchar(int char);
+int _putchar(int char);
 #endif
